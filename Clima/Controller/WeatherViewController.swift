81ce279b81ec42238e4ced38745d04142faedb86
Archit Garg <archit.g1998@gmail.com>
2020-02-22 19:14:56 +0530

    App is ready to use

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import CoreLocation
 
-class WeatherViewController: UIViewController, UITextFieldDelegate, WeatherManagerDelegate {
+class WeatherViewController: UIViewController {
     
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
@@ -16,42 +17,30 @@ class WeatherViewController: UIViewController, UITextFieldDelegate, WeatherManag
     @IBOutlet weak var searchTextField: UITextField!
     
     var weatherManager = WeatherManager()
+    let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestLocation()
+        
         weatherManager.delegate = self
         searchTextField.delegate = self
     }
     
-    
-    @IBAction func searchPressed(_ sender: UIButton) {
-        searchTextField.endEditing(true)
-        print(searchTextField.text!)
+    @IBAction func getCoreLocation(_ sender: UIButton) {
+        locationManager.requestLocation()
     }
     
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        searchTextField.endEditing(true)
-        print(searchTextField.text!)
-        return true
-    }
     
-    func textFieldDidEndEditing(_ textField: UITextField) {
-        if let city = searchTextField.text {
-            searchTextField.text = ""
-            weatherManager.fetchWeather(cityName: city)
-        }
-    }
-    
-    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
-        if textField.text! != "" {
-            textField.placeholder = "Search"
-            return true
-        } else {
-            textField.placeholder = "Type Something"
-            return false
-        }
-    }
+}
+
+//MARK: - WeatherManagerDelegate
+
+extension WeatherViewController : WeatherManagerDelegate {
     
     func didUpdateWeather(_ weatherManager: WeatherManager, weather : WeatherModel) {
         DispatchQueue.main.async {
@@ -64,6 +53,55 @@ class WeatherViewController: UIViewController, UITextFieldDelegate, WeatherManag
     func didFailWithError(error: Error) {
         print(error)
     }
-
+    
 }
 
+//MARK: - UITextFieldDelegate
+
+extension WeatherViewController : UITextFieldDelegate {
+    
+    @IBAction func searchPressed(_ sender: UIButton) {
+           searchTextField.endEditing(true)
+           print(searchTextField.text!)
+       }
+       
+       func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+           searchTextField.endEditing(true)
+           print(searchTextField.text!)
+           return true
+       }
+       
+       func textFieldDidEndEditing(_ textField: UITextField) {
+           if let city = searchTextField.text {
+               searchTextField.text = ""
+               weatherManager.fetchWeather(cityName: city)
+           }
+       }
+       
+       func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+           if textField.text! != "" {
+               textField.placeholder = "Search"
+               return true
+           } else {
+               textField.placeholder = "Type Something"
+               return false
+           }
+       }
+}
+
+//MARK: - CLLocationManagerDelegate
+
+extension WeatherViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            locationManager.stopUpdatingLocation()
+            let lat = location.coordinate.latitude
+            let lon = location.coordinate.longitude
+            weatherManager.fetchWeather(latitude: lat, longitude: lon)
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
+}
